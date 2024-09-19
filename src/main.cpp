7abb79550f038f0b620701497681b5d8f0a9d0d8
KarolOrszulik <orkarox@gmail.com>
2024-09-19 17:04:23 +0200

    Added large clock mode

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,23 @@ Frame_Date dateFrame({2, 46}, {60, 17});
 
 Frame *frames[] = {&tempFrame, &humidFrame, &clockFrame, &dateFrame};
 
+NetworkVariable varRadius{"radius"};
+
+#define NUM_POINTS 180
+struct Point {int x; int y;};
+Point points[NUM_POINTS] = {
+    {0, -32}, {1, -32}, {2, -32}, {3, -32}, {4, -32}, {5, -32},
+    {6, -31}, {7, -31}, {8, -31}, {9, -31},
+    {10, -30}, {11, -30},
+    {12, -29}, {13, -29}, {14, -29},
+    {15, -28},
+    {16, -27}, {17, -27},
+    {18, -26},
+    {19, -25}, {20, -25}, 
+    {21, -24},
+    {22, -23}
+};
+
 void setup()
 {
     Serial.begin(115200);
@@ -37,7 +54,40 @@ void setup()
 
     varLeft.init();
     varRight.init();
+
+    varRadius.init();
+
+    // mirror the first octant to the second
+    for (int i = 0; i < 22; i++)
+    {
+        points[44-i].x = -points[i].y - 1;
+        points[44-i].y = -points[i].x - 1;
+    }
+
+    // mirror first quarter to the fourth
+    for (int i = 0; i < 45; i++)
+    {
+        points[89-i].x = points[i].x;
+        points[89-i].y = -points[i].y - 1;
+    }
+
+    for (int i = 0; i < 90; i++)
+    {
+        points[179-i].x = -points[i].x - 1;
+        points[179-i].y = points[i].y;
+    }
+
+    // offset points
+    for (Point &point : points)
+    {
+        point.x += 32;
+        point.y += 32;
+    }
+
+
 }
+
+#define UPDATES_PER_SECOND (NUM_POINTS/60)
 
 void loop()
 {
@@ -46,14 +96,58 @@ void loop()
     g_webServer.handleClient();
     g_timeInfo = NTP_getTime();
 
-    if (last_second == g_timeInfo.tm_sec)
-        return;
+    static int activePixels = 0;
+    static uint32_t nextUpdate = 0;
+    static int updatesThisSecond = 0;
+    const  uint32_t now = millis();
 
+    if (last_second == g_timeInfo.tm_sec)
+    {
+        if (now >= nextUpdate && updatesThisSecond < UPDATES_PER_SECOND)
+        {
+            nextUpdate = now + 1000 / UPDATES_PER_SECOND;
+            ++updatesThisSecond;
+        }
+        else
+        {
+            return;
+        }
+    }
+    else
+    {
+        activePixels = g_timeInfo.tm_sec * UPDATES_PER_SECOND;
+        last_second = g_timeInfo.tm_sec;
+        updatesThisSecond = 0;
+    }
 
     g_display.clearDisplay();
 
-    for (Frame *frame : frames)
-        frame->show();
+    // for (Frame *frame : frames)
+    //     frame->show();
+
+    g_display.setTextColor(g_colorPrimary);
+    g_display.setTextSize(3);
+    g_display.setCursor(15, 9);
+    if (g_timeInfo.tm_hour < 10)
+        g_display.print("0");
+    g_display.print(g_timeInfo.tm_hour);
+    g_display.setCursor(15, 33);
+    if (g_timeInfo.tm_min < 10)
+        g_display.print("0");
+    g_display.print(g_timeInfo.tm_min);
+
+    int radius = varRadius._value.toInt();
+
+    uint16_t colorA = g_timeInfo.tm_min & 1 ? g_colorPrimary : 0;
+    uint16_t colorB = g_timeInfo.tm_min & 1 ? 0 : g_colorPrimary;
+    
+    for (int i = 0; i < NUM_POINTS; i++)
+        g_display.drawPixel(points[i].x, points[i].y, colorA);
+
+    for (int i = 0; i < activePixels; i++)
+        g_display.drawPixel(points[i].x, points[i].y, colorB);
+
+    ++activePixels %= NUM_POINTS + 1;
 
     g_display.showBuffer();
 }
